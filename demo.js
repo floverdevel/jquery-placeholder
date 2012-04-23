c74e11778b3d52fbba599abefb3fa32ee2943b80
Ellis Antaya <eantaya@lapresse.ca>
2012-04-23 15:48:34 -0400

    amelioration de la page demo

--- demo.js
+++ demo.js
@@ -4,13 +4,11 @@
 $(document).ready(function() {
 
     $('#xhtml input[type=text], #xhtml input[type=password]').placeholder({
-        'background' : '#ff0000',
-        'color' : '#00ff00',
         'text' : {
-            'username1' : 'Doit &ecirc;tre unique',
-            'password1' : 'mot de passe',
-            'lastname1' : 'nom de famille',
-            'firstname1' : 'pr&eacute;nom'
+            'username1' : 'Nom d\'usager',
+            'password1' : '... fonctionne aussi avec les input de type "password" ...',
+            'lastname1' : '... fonctionne aussi avec du texte trop long qui est tronqué pour ne pas dépasser le input qui lui est associé',
+            'firstname1' : '.. et même avec un champ qui était pré-rempli'
         }
     });
 });
